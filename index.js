ceef18a8638dea2538d63f45830563da91362ed9
Paulo Diovani <paulo@diovani.com>
2021-04-26 18:32:22 -0300

    Add discord package and sample commands

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const hello = `hello, language is ${process.env.LANGUAGE}`
+import { debug } from './lib/logger.js'
+import { getApp, login, onReady } from './lib/discord-client.js'
+import { getCommands, registerCommands } from './lib/commands/index.js'
 
-console.log(hello)
+login(process.env.TOKEN)
+
+onReady(async () => {
+  const app = () => getApp(process.env.GUILD)
+
+  registerCommands(app)
+
+  const commands = await getCommands(app)
+  debug('Registered commands: ', commands)
+})
