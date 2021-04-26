de9ab163633ffa7f3e17b0e4dc824f84de3ac64e
Paulo Diovani <paulo@diovani.com>
2021-04-26 20:42:03 -0300

    Add sample commands and some test moves

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+import path from 'path'
 import { debug } from './lib/logger.js'
+import { fileURLToPath } from 'url'
 import { getApp, login, onReady } from './lib/discord-client.js'
-import { getCommands, registerCommands } from './lib/commands/index.js'
+import { getCommands, registerCommands } from './lib/commands.js'
 
 login(process.env.TOKEN)
 
 onReady(async () => {
+  const dirname = path.dirname(fileURLToPath(import.meta.url))
+  const localeFile = path.join(dirname, 'locales', process.env.LOCALE + '.yml')
+  debug('Using locale file at', localeFile)
+
   const app = () => getApp(process.env.GUILD)
 
-  registerCommands(app)
+  registerCommands(app, localeFile)
 
   const commands = await getCommands(app)
   debug('Registered commands: ', commands)
