4651f133e226f826e6ec0a71db6f072fd054a5a2
Paulo Diovani <paulo@diovani.com>
2021-04-27 18:05:26 -0300

    Refactor and add interactions and embed

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 import path from 'path'
-import { debug } from './lib/logger.js'
+import { debug, info } from './lib/logger.js'
 import { fileURLToPath } from 'url'
-import { getApi, getApp, login, onInteraction, onReady } from './lib/discord-client.js'
-import { getCommands, registerCommands, registerInteractions } from './lib/commands.js'
+import { getClient, getApp } from './lib/discord-client.js'
+import { getCommands, registerCommands } from './lib/commands.js'
+import { waitForInteractions } from './lib/interactions.js'
+import { loadLocales } from './lib/locales.js'
 
-login(process.env.TOKEN)
+const dirname = path.dirname(fileURLToPath(import.meta.url))
+const localeFile = path.join(dirname, 'locales', process.env.LOCALE + '.yml')
+debug('Using locale file at', localeFile)
 
-onReady(async () => {
-  const dirname = path.dirname(fileURLToPath(import.meta.url))
-  const localeFile = path.join(dirname, 'locales', process.env.LOCALE + '.yml')
-  debug('Using locale file at', localeFile)
+const locales = loadLocales(localeFile)
 
+const client = getClient()
+
+client.on('ready', async () => {
   const app = () => getApp(process.env.GUILD)
-  const api = getApi()
 
-  // register new commands
-  await registerCommands(app, localeFile)
+  // register commands
+  await registerCommands(locales)
 
   // list commands in debug mode
   const commands = await getCommands(app)
-  debug('Registered commands: ', JSON.stringify(commands, null, 2))
+  info(`Registered ${commands.length} commands`)
+  debug('Registered commands: ', JSON.stringify(commands))
 
-  // register interactions
-  await registerInteractions(api, onInteraction, localeFile)
+  // wait for received interactions
+  waitForInteractions(locales, process.env.EMBED_COLOR)
+
+  // all done
+  info('Bot is ready! 🤖')
 })
+
+client.login(process.env.TOKEN)
