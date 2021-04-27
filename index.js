c020c038dc142b8e702a9f178a442a60d4694ccd
Paulo Diovani <paulo@diovani.com>
2021-04-27 19:23:20 -0300

    Add dice rolls and fix guild

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import path from 'path'
 import { debug, info } from './lib/logger.js'
 import { fileURLToPath } from 'url'
-import { getClient, getApp } from './lib/discord-client.js'
+import { getClient, getApp as _getApp } from './lib/discord-client.js'
 import { getCommands, registerCommands } from './lib/commands.js'
 import { waitForInteractions } from './lib/interactions.js'
 import { loadLocales } from './lib/locales.js'
+import { DICE_2D6 } from './lib/constants.js'
+
+const config = {
+  LOCALE: process.env.LOCALE || 'en_US',
+  COLOR: process.env.EMBED_COLOR || '#000',
+  GUILD: process.env.GUILD,
+  TOKEN: process.env.TOKEN,
+  DICE: process.env.DICE || DICE_2D6,
+}
 
 const dirname = path.dirname(fileURLToPath(import.meta.url))
-const localeFile = path.join(dirname, 'locales', process.env.LOCALE + '.yml')
+const localeFile = path.join(dirname, 'locales', config.LOCALE + '.yml')
 debug('Using locale file at', localeFile)
 
 const locales = loadLocales(localeFile)
@@ -15,21 +24,21 @@ const locales = loadLocales(localeFile)
 const client = getClient()
 
 client.on('ready', async () => {
-  const app = () => getApp(process.env.GUILD)
+  const getApp = () => _getApp(config.GUILD)
 
   // register commands
-  await registerCommands(locales)
+  await registerCommands(getApp, locales)
 
   // list commands in debug mode
-  const commands = await getCommands(app)
+  const commands = await getCommands(getApp)
   info(`Registered ${commands.length} commands`)
   debug('Registered commands: ', JSON.stringify(commands))
 
   // wait for received interactions
-  waitForInteractions(locales, process.env.EMBED_COLOR)
+  waitForInteractions(locales, config.DICE, config.COLOR)
 
   // all done
   info('Bot is ready! 🤖')
 })
 
-client.login(process.env.TOKEN)
+client.login(config.TOKEN)
